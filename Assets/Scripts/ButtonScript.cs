2939ac5f34940b9272642a87a92219b093f825b9
trufYm <emmanuel_john_taylan@dlsu.edu.ph>
2024-07-29 16:32:01 +0800

    more screens

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,21 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    public void SwitchScene()
+    [SerializeField] GameObject controlPanel;
+    [SerializeField] GameObject objectivePanel;
+
+    public void LoadGame()
     {
-        switch(this.name)
-        {
-            case "Start":
-                SceneManager.LoadScene("GameScene");
-                break;
-            case "How to Play":
-                SceneManager.LoadScene("Tutorial");
-                break;
-        }
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SwitchControlPanel()
+    {
+        if(this.controlPanel.activeInHierarchy) this.controlPanel.SetActive(false);
+        else this.controlPanel.SetActive(true);
+    }
+
+    public void SwitchObjectivePanel()
+    {
+        if (this.objectivePanel.activeInHierarchy) this.objectivePanel.SetActive(false);
+        else this.objectivePanel.SetActive(true);
     }
 }
