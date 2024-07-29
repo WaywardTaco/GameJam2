b91508c9b1be03aa6d73964064b38af179e7a01d
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-07-29 12:12:33 +0800

    door open sound

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -6,16 +6,22 @@ public class DoorScript : MonoBehaviour
 {
     [SerializeField] private string OpenTriggerEvent = "";
     [SerializeField] private string CloseTriggerEvent = "";
+    [SerializeField] private AudioClip openSound;
+    private AudioSource audioSource;
 
     private void Start() {
         if (OpenTriggerEvent != "")
             EventBroadcaster.Instance.AddObserver(OpenTriggerEvent, OpenDoor);
         if (CloseTriggerEvent != "")
             EventBroadcaster.Instance.AddObserver(CloseTriggerEvent, CloseDoor);
+
+        this.audioSource = GetComponent<AudioSource>();
+        this.audioSource.clip = this.openSound;
     }
 
     private void OpenDoor(){
         this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        this.audioSource.Play();
     }
 
     private void CloseDoor(){
