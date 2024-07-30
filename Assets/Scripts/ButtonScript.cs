5a07f628a201600ed121b1ef7193f510dbb35b42
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-07-30 13:00:37 +0800

    menu fixes

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -7,48 +7,65 @@ public class ButtonScript : MonoBehaviour
 {
     [SerializeField] GameObject TitleCanvas;
     [SerializeField] GameObject TutorialCanvas;
-    [SerializeField] GameObject controlPanel;
-    [SerializeField] GameObject objectivePanel;
+    [SerializeField] private AudioClip[] clickSounds;
 
     private void Start() {
         TitleCanvas.SetActive(true);
-        controlPanel.SetActive(false);
-        objectivePanel.SetActive(false);
+        // controlPanel.SetActive(false);
+        // objectivePanel.SetActive(false);
         TutorialCanvas.SetActive(false);
     }
 
     public void LoadGame()
     {
+        PlayClickSound();
         SceneManager.LoadScene("GameScene");
     }
 
     public void LoadTutorial()
     {
         // SceneManager.LoadScene("Tutorial");
+        PlayClickSound();
         this.TitleCanvas.SetActive(false);
         this.TutorialCanvas.SetActive(true);
     }
     public void LoadTitleScreen()
     {
         // SceneManager.LoadScene("TitleScreen");
+        PlayClickSound();
         this.TutorialCanvas.SetActive(false);
         this.TitleCanvas.SetActive(true);
     }
 
     public void QuitGame()
     {
+        PlayClickSound();
         Application.Quit();
     }
 
-    public void SwitchControlPanel()
+    private void PlayClickSound()
     {
-        if(this.controlPanel.activeInHierarchy) this.controlPanel.SetActive(false);
-        else this.controlPanel.SetActive(true);
+        AudioSource m_AudioSource = this.gameObject.GetComponent<AudioSource>();
+        // pick & play a random click sound from the array,
+        // excluding sound at index 0
+        int n = Random.Range(1, clickSounds.Length);
+        m_AudioSource.clip = clickSounds[n];
+        m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        // move picked sound to index 0 so it's not picked next time
+        clickSounds[n] = clickSounds[0];
+        clickSounds[0] = m_AudioSource.clip;
     }
 
-    public void SwitchObjectivePanel()
-    {
-        if (this.objectivePanel.activeInHierarchy) this.objectivePanel.SetActive(false);
-        else this.objectivePanel.SetActive(true);
-    }
+
+    // public void SwitchControlPanel()
+    // {
+    //     if(this.controlPanel.activeInHierarchy) this.controlPanel.SetActive(false);
+    //     else this.controlPanel.SetActive(true);
+    // }
+
+    // public void SwitchObjectivePanel()
+    // {
+    //     if (this.objectivePanel.activeInHierarchy) this.objectivePanel.SetActive(false);
+    //     else this.objectivePanel.SetActive(true);
+    // }
 }
