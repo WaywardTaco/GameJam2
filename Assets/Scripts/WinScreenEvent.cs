c6eeac4776f26c0a11c31eaa4a197ee63198ba0f
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-07-30 15:17:10 +0800

    yes

--- Assets/Scripts/WinScreenEvent.cs
+++ Assets/Scripts/WinScreenEvent.cs
@@ -10,6 +10,7 @@ public class WinScreenEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(DoNotDestroyOnLoad.Instance.gameObject);
         this.StartCoroutine(this.WaitTransitionToTitle());
     }
 
